62709d6b2c4075be7d7ab7612054cfc338b34f0e
venu410 <venukumarmaddela@gmail.com>
2022-10-31 00:11:04 +0530

    Update ProductServiceApplication.java

--- src/main/java/venu/in/coding/microservices/product/ProductServiceApplication.java
+++ src/main/java/venu/in/coding/microservices/product/ProductServiceApplication.java
@@ -2,10 +2,10 @@ package venu.in.coding.microservices.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
